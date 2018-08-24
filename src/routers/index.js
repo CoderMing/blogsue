8dd272c3d18f62deb7298cc672213e4d8730a3d2
coderming <coderming@foxmail.com>
2018-08-24 23:14:56 +0800

    fix: fix typo

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Gitalk from '../components/Gitalk'
 import Article from '../components/Article'
