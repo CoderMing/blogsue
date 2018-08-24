8860d8c842c75fb464baad39df93d25b44cec87f
coderming <coderming@foxmail.com>
2018-08-24 09:35:11 +0800

    chore: 修改注释

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,5 @@
 /**
  * 中文化 commitlit 规范
- * changelog 只会显示 feat 和 fix 的msg
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
--- src/common/configureStore.js
+++ src/common/configureStore.js
@@ -5,8 +5,6 @@ const middlewares = [
   logger
 ]
 
-// Ques: compose第二个参数作用？
-// Ans: 无作用，原因为我未理解compose的内容
 export default compose(
   applyMiddleware(...middlewares)
 )(createStore)
