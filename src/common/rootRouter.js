771ddf917fdcb11d95ea29071591082241f7232a
coderming <coderming@foxmail.com>
2018-08-26 16:28:20 +0800

    feat: 引入blueprint UI组件库

--- src/common/rootRouter.js
+++ src/common/rootRouter.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from '../features/nav'
 import Gitalk from '../features/gitalk/Gitalk'
 import Article from '../features/article/Article'
+import { Button } from '@blueprintjs/core'
+
+import './global.styl'
 
 export default () => (
   <Router>
@@ -11,6 +14,7 @@ export default () => (
       <Route path="/" component={Header} />
       <Route exact path="/" component={Gitalk} />
       <Route path="/article/:id" component={Article} />
+      <Button intent="success" text="button content" />
     </div>
   </Router>
 )
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom'
 import registerServiceWorker from './common/registerServiceWorker'
 import configureStore from './common/configureStore'
 import reducer from './reducers/index'
-import App from './App.jsx'
+import RootRouter from './common/rootRouter'
 
 const store = configureStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <RootRouter />
   </Provider>,
   document.getElementById('root')
 )
