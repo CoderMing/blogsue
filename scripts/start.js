cb0577e56f15b8db321b7229fa13d612d4e0c8c9
coderming <coderming@foxmail.com>
2018-09-01 14:03:03 +0800

    chore: 去除掉生产环境中的redux-logger

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,3 @@
-
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development'
 process.env.NODE_ENV = 'development'
@@ -86,7 +84,6 @@ choosePort(HOST, DEFAULT_PORT)
       console.log(chalk.cyan('Starting the development server...\n'))
       openBrowser(urls.localUrlForBrowser)
     })
-
     ;['SIGINT', 'SIGTERM'].forEach(function(sig) {
       process.on(sig, function() {
         devServer.close()
--- src/common/configureStore.js
+++ src/common/configureStore.js
@@ -1,6 +1,10 @@
 import logger from 'redux-logger'
 import { createStore, applyMiddleware, compose } from 'redux'
 
-const middlewares = [logger]
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
 
 export default compose(applyMiddleware(...middlewares))(createStore)
