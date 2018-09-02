4029719809074226880ec39e586310c7bc15cdbd
coderming <coderming@foxmail.com>
2018-09-02 10:45:16 +0800

    feat: 移动端适配

--- src/features/UserSection/scrollAni.js
+++ src/features/UserSection/scrollAni.js
@@ -1,29 +1,31 @@
 export default () => {
-  const el = document.querySelector('.content-right .section-item:first-of-type')
-  const secEl = window.document.querySelector('.content-right')
-  const elComHeight = `${window.parseInt(window.getComputedStyle(el).height) + 20}px`
-  const navEl = window.document.querySelector('#header-container')
+  if (window.innerWidth > 800) {
+    const el = document.querySelector('.content-right .section-item:first-of-type')
+    const secEl = window.document.querySelector('.content-right')
+    const elComHeight = `${window.parseInt(window.getComputedStyle(el).height) + 20}px`
+    const navEl = window.document.querySelector('#header-container')
 
-  if (!(el && secEl && navEl && elComHeight)) return
+    if (!(el && secEl && navEl && elComHeight)) return
 
-  let prevPageY = 0
-  const loopFunc = () => {
-    const pageY = window.pageYOffset
-    if (prevPageY !== pageY) {
-      prevPageY = pageY
-      if (pageY <= 100) {
-        el.style.transform = `translate(0, 0)`
-        secEl.style.transform = `translate(0, 0)`
-        navEl.style.opacity = 1
-        navEl.className = 'bp3-dark'
-      } else {
-        el.style.transform = `translate(0, -100px)`
-        secEl.style.transform = `translate(0, -${elComHeight})`
-        navEl.className = ''
-        navEl.style.opacity = 0.9
+    let prevPageY = 0
+    const loopFunc = () => {
+      const pageY = window.pageYOffset
+      if (prevPageY !== pageY) {
+        prevPageY = pageY
+        if (pageY <= 100) {
+          el.style.transform = `translate(0, 0)`
+          secEl.style.transform = `translate(0, 0)`
+          navEl.style.opacity = 1
+          navEl.className = 'bp3-dark'
+        } else {
+          el.style.transform = `translate(0, -100px)`
+          secEl.style.transform = `translate(0, -${elComHeight})`
+          navEl.className = ''
+          navEl.style.opacity = 0.9
+        }
       }
+      setTimeout(loopFunc, 100)
     }
-    setTimeout(loopFunc, 100)
+    loopFunc()
   }
-  loopFunc()
 }
