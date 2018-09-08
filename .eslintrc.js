7d850616b45caa7bce1d64938be8af220163adf8
coderming <coderming@foxmail.com>
2018-09-08 16:30:20 +0800

    chore: 修改eslint的模式

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: 'react-app',
   plugins: [ 'prettier' ],
   rules: {
-    'prettier/prettier': process.env.NODE_ENV ? 'warn' : 'error'
+    'prettier/prettier': 'warn'
   }
 }
