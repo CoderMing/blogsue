d531a094c8ddcc08c6b842b5d131eae80d80cac4
coderming <coderming@foxmail.com>
2018-09-08 16:55:31 +0800

    docs: 修改example config

---
+++ src/config/index-example.js
@@ -0,0 +1,100 @@
+import pageConfApi from '../common/pageConfApi'
+import pageConf from './pageConf'
+
+const config = {
+  title: 'aha的博客',
+
+  titleSuffix: 'Blogsue',
+
+  description: '副标题',
+
+  homePage: 'https://coderming.github.io',
+
+  userLinks: [
+    {
+      title: 'GitHub',
+      icon: 'user',
+      url: 'https://github.com/coderming'
+    }
+  ],
+  /**
+   * 仓库如果在用户下，填 users
+   * 如果在组织下，填 orgs
+   */
+  userType: 'users',
+  /**
+   * issue 仓库所在地
+   */
+  user: 'coderming',
+
+  repo: 'blog',
+  // 请申请一个clientID
+  clientID: '',
+  clientSecret: '',
+  /**
+   * 在本博客显示的其他issue创建者
+   * 项目所有者（组织仓库的话为所有组织成员）及COLLABORATOR默认会显示
+   * 主要目的是为了方式他人恶意发issue污染博客内容
+   */
+  otherAuthors: [''],
+
+  /**
+   * Gitalk 配置项
+   * 请不要填写 owner，repo，clientID，clientSecret 字段
+   * 会自动引用本config的 user，repo，clientID，clientSecret 字段以对应
+   * 如何配置请访问 https://github.com/gitalk/gitalk/blob/master/readme-cn.md
+   */
+  gitalk: {
+    distractionFreeMode: false
+  },
+
+  navItem: [
+    {
+      name: 'Github',
+      url: 'https://github.com/xxx',
+      icon: 'person'
+    },
+    {
+      name: 'ZHIHU',
+      icon: 'user',
+      url: 'https://www.zhihu.com/people/xxx/activities'
+    }
+  ],
+
+  /**
+   * Github 用户配置项
+   */
+  linkSections: [
+    {
+      title: '友情链接',
+      linkList: [
+        {
+          name: 'CoderMing的个人博客',
+          href: 'https://coderming.com/'
+        }
+      ]
+    }
+  ],
+  /**
+   * 右侧sections的空白处
+   * 注意！此处是直接插入html字段，目的是为了支持更高的用户自定义度（例如使用a标签
+   * 请勿插入危险的标签
+   */
+  blankSections: ['Write with ❤️ by xxx'],
+
+  footers: [
+    '本博客系统由 <a href="http://www.reactjs.org">React.js</a> 驱动',
+    '评论系统由 <a href="https://github.com/gitalk/gitalk">Gitalk</a> 驱动'
+  ],
+  /**
+   * 是否使用serviceWorker缓存文件
+   * 如果你的文件将会经常变动，或者是在同一域名下有其它项目在运行，请填false
+   */
+  isUseServiceWorker: false,
+
+  pageConf,
+
+  pageConfApi
+}
+
+export default config
