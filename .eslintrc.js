1c9582f337abce6522380f5a58e375626634662e
coderming <coderming@foxmail.com>
2018-09-25 11:56:43 +0800

    chore: 修改eslint的报错规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: 'react-app',
   plugins: [ 'prettier' ],
-  rules: {
-    'prettier/prettier': 'error'
+  rules: { 
+    'prettier/prettier': process.env.NODE_ENV === 'production' ? 'warn': 'error'
   }
 }
